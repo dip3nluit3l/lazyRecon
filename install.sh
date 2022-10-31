a9472a2f8d6dc199b7be8b6c8817e1a032150197
Dipen Luitel <46955586+dip3nluit3l@users.noreply.github.com>
2022-11-01 01:09:52 +0545

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-cd lazyHunter
 chmod +x *
 pip3 install -r requirements.txt
 sudo cp  lazyHunter /usr/bin
 cd ..
-sudo cp lazyHunter /opt/
+cp lazyHunter /opt/
 cd
 echo "Sucessfully installed!!! Now Run, lazyHunter targetDomain.com controller-Key"
 echo "Example: lazyHunter dipenluitel30.com.np"
